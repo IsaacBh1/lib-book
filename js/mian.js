4480417b96f5cfdb4c91e82870d0da6f914a7e35
IsaacBh1 <isaac2548623@gmail.com>
2024-10-22 22:05:57 +0100

    adding filter buttons to books window

--- js/mian.js
+++ js/mian.js
@@ -1,5 +1,26 @@
 const searhbutton = document.querySelector('.search-button');
+const searchtxtbox = document.querySelector("#searchbook") ;
+const navbar = document.querySelector(".filter-element") ; 
+const filter_fields = document.querySelectorAll(".filter-element") ; 
+
+
 searhbutton.addEventListener('click' , ()=>{
-    console.log('loop clicked') ; 
-    const navElements = document.querySelector(".nav-elemnts").classList.add('hide');    
+    navbar.classList.add('hide');
+    searchtxtbox.classList.remove('hide') ; 
+    searchtxtbox.focus();  
 }) 
+
+searchtxtbox.addEventListener('blur' ,()=> {
+    navbar.classList.remove('hide');    
+    searchtxtbox.classList.add('hide');    
+});
+
+filter_fields.forEach((button) => {
+    button.addEventListener('click', function () {
+      // Remove 'active' class from all buttons
+      filter_fields.forEach((btn) => btn.classList.remove('selected'));
+  
+      // Add 'active' class to the clicked button
+      button.classList.add('selected');
+    });
+  });
